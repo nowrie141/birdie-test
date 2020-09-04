7c77aaac3903a08a3302651eaf2442030f547569
nowrie141 <i62abgai@uco.es>
2020-09-04 08:58:43 +0100

    Express API done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,39 @@
-import app from './application';
+import { Server } from './Server';
+import * as express from 'express';
 
-const port = process.env.PORT || 8000;
+const app = express();
 
-app.listen(port, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`Server started at http://localhost:${port}`);
-});
+const port = 8000
+
+var mysql = require('mysql')
+
+var connection = mysql.createPool({
+  host: 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
+  port: '3306',
+  user: 'test-read',
+  password: 'xnxPp6QfZbCYkY8',
+  database: 'birdietest'
+})
+
+const sql = 'SELECT * FROM events'
+
+app.set('port', port)
+
+app.use(function (_req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header("Access-Control-Allow-Methods", 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+})
+
+app.get('/data', function (_req: any, res: any, _next: any) {
+  connection.query(sql, function (err: any, result: any, _fields: any) {
+    if (err) throw err
+    const jsonData = JSON.parse(JSON.stringify(result));
+    console.log("jsonData", jsonData);
+    res.send({ data: result })
+  })
+})
+
+const server = new Server(app);
+server.start(port)
--- front-end/src/components/app/App.tsx
+++ front-end/src/components/app/App.tsx
@@ -1,21 +1,20 @@
 import * as React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import { RootState } from '@App/store/reducers';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
+import { Observation } from '@App/types/Observations';
+// import Title from '@App/components/Title';
+// import Logo from '@App/components/Logo';
+// import SubTitle from '@App/components/SubTitle';
 
-import Title from '@App/components/Title';
-import Logo from '@App/components/Logo';
-import SubTitle from '@App/components/SubTitle';
-
-const LogoUrl = require('../../assets/images/logo-birdie.svg');
+// const LogoUrl = require('../../assets/images/logo-birdie.svg');
 
 interface AppProps {
-
 }
 
 interface AppState {
-
+  observations: Observation[]
 }
 
 const GlobalStyle = createGlobalStyle`
@@ -28,16 +27,28 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const AppContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
+// const AppContainer = styled.div`
+//  width: 100%;
+//  height: 100%;
+//  display: flex;
+//  justify-content: center;
+//  align-items: center;
+//  flex-direction: column;
+// `;
 
 class App extends React.Component<AppProps, AppState> {
+  state: AppState = {
+    observations: [],
+  };
+
+  componentDidMount(){
+    fetch('http://localhost:8000')
+    .then( res => res.json())
+    .then(res => this.setState({
+      observations: res.data
+    }))
+  }
+
   public constructor(props: AppProps) {
     super(props);
   }
@@ -46,18 +57,14 @@ class App extends React.Component<AppProps, AppState> {
     return (
       <>
         <GlobalStyle />
-        <AppContainer>
-          <Logo src={LogoUrl} />
-          <Title>Welcome to the birdie test</Title>
-          <SubTitle>Best of luck!</SubTitle>
-        </AppContainer>
+    <div className="App-intro">{observations}</div>
       </>
     );
   }
 }
 
-const mapStateToProps = (state: RootState, ownProps: object) => {};
+const mapStateToProps = (state: RootState, ownProps: object) => { };
 
-const mapDispatchToProps = (dispatch: Dispatch<RootState>) => {};
+const mapDispatchToProps = (dispatch: Dispatch<RootState>) => { };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
